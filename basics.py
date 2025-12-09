be5dda72120541eab2114505b4fecb6a10c44bd7
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-12-09 08:06:37 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -842,8 +842,8 @@ n = 5
 for i in range(1, n+1):
     print(" "*(n-i) + "*"*(2*i-1))
 
-# n = 5
-# for i in range(1, n+1):
-#     print(" "*(n-i) + "*"*(2*i-1))
+n = 5
+for i in range(1, n+1):
+    print(" "*(n-i) + "*"*(2*i-1))
 
 
