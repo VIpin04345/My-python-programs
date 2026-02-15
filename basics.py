fe88b1b25d0d70d9d8cc9477d0bed215e5a3387c
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-15 10:30:32 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -933,3 +933,15 @@ c=25
 if c>5:
     raise MyException('something went wrong')
 
+
+
+
+1️⃣ Linear Search (Basic)
+def linear_search(arr, key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            return i
+    return -1
+
+arr = [10, 20, 30, 40, 50]
+print(linear_search(arr, 30))
