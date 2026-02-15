aa8bac0265d393b7489364eecd42ae050175129e
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-15 10:32:37 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1002,3 +1002,20 @@ def jump_search(arr, key):
         if arr[i] == key:
             return i
     return -1
+
+
+
+5️⃣ Interpolation Search
+def interpolation_search(arr, key):
+    low, high = 0, len(arr)-1
+
+    while low <= high and key >= arr[low] and key <= arr[high]:
+        pos = low + ((key-arr[low])*(high-low))//(arr[high]-arr[low])
+
+        if arr[pos] == key:
+            return pos
+        elif arr[pos] < key:
+            low = pos + 1
+        else:
+            high = pos - 1
+    return -1
