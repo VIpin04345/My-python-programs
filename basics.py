f21d02828ca3b68414440056272727a3dc61b64b
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-16 12:07:24 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1025,13 +1025,13 @@ def interpolation_search(arr, key):
 
 # 🔄 Sorting Algorithms (5 Programs)
 # 1️⃣ Bubble Sort (Basic)
-# def bubble_sort(arr):
-#     n = len(arr)
-#     for i in range(n):
-#         for j in range(n-i-1):
-#             if arr[j] > arr[j+1]:
-#                 arr[j], arr[j+1] = arr[j+1], arr[j]
-#     return arr
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
 # print(bubble_sort([5,3,8,1]))
 
