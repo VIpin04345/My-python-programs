6dcdd727a4f4338059fc0b781504b6f519669aa8
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-16 12:08:47 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1060,30 +1060,30 @@ print(bubble_sort([5,3,8,1]))
 # 👉 Small data me fast.
 
 # 4️⃣ Merge Sort (Intermediate)
-# def merge_sort(arr):
-#     if len(arr) > 1:
-#         mid = len(arr)//2
-#         left = arr[:mid]
-#         right = arr[mid:]
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
 
-#         merge_sort(left)
-#         merge_sort(right)
+        merge_sort(left)
+        merge_sort(right)
 
-#         i=j=k=0
-#         while i<len(left) and j<len(right):
-#             if left[i] < right[j]:
-#                 arr[k]=left[i]; i+=1
-#             else:
-#                 arr[k]=right[j]; j+=1
-#             k+=1
+        i=j=k=0
+        while i<len(left) and j<len(right):
+            if left[i] < right[j]:
+                arr[k]=left[i]; i+=1
+            else:
+                arr[k]=right[j]; j+=1
+            k+=1
 
-#         while i<len(left):
-#             arr[k]=left[i]; i+=1; k+=1
+        while i<len(left):
+            arr[k]=left[i]; i+=1; k+=1
 
-#         while j<len(right):
-#             arr[k]=right[j]; j+=1; k+=1
+        while j<len(right):
+            arr[k]=right[j]; j+=1; k+=1
 
-#     return arr
+    return arr
 
 # 5️⃣ Quick Sort
 def quick_sort(arr):
