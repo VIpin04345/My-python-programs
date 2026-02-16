c1de59f81c3b53438d50ab998c54838e3fba55bd
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-16 12:09:09 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1046,15 +1046,15 @@ print(bubble_sort([5,3,8,1]))
 #     return arr
 
 # 3️⃣ Insertion Sort
-# def insertion_sort(arr):
-#     for i in range(1, len(arr)):
-#         key = arr[i]
-#         j = i-1
-#         while j >= 0 and arr[j] > key:
-#             arr[j+1] = arr[j]
-#             j -= 1
-#         arr[j+1] = key
-#     return arr
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
 
 
 # 👉 Small data me fast.
