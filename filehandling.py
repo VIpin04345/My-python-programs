ea25e643faaa4be6b2930d2de3943f6bba9a39ce
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-17 15:04:07 +0530

    Update filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -4,3 +4,11 @@ f.write("Hello, this is my first file handling program.")
 f.close()
 
 print("File created and data written.")
+
+
+
+🟢 2. File Read Program
+f = open("demo.txt", "r")
+data = f.read()
+print(data)
+f.close()
