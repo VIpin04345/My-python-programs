07e3c5a57ea3221174dff90b160d76fd141b0d69
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-22 14:13:15 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1143,3 +1143,17 @@ class Employee(Person):
 e1 = Employee("Amit", 50000)
 e1.show()
 e1.show_salary()
+
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance   # private variable
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = BankAccount(1000)
+acc.deposit(500)
+print("Balance:", acc.get_balance())
