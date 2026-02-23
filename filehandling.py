90a1052d2773d7b60f425dd687863b1dcef4f7de
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-23 13:58:21 +0530

    Update filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -110,3 +110,13 @@ n = 3
 with open("demo.txt", "r") as f:
     for i in range(n):
         print(f.readline().strip())
+
+# 🔵 Merge Two Files
+with open("file1.txt", "r") as f1, open("file2.txt", "r") as f2:
+    data1 = f1.read()
+    data2 = f2.read()
+
+with open("merged.txt", "w") as f:
+    f.write(data1 + "\n" + data2)
+
+print("Files merged successfully.")
