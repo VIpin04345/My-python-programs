9943cda485e5396fe510c9285bf23fa4d2e7e64b
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-04 09:43:02 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -583,7 +583,17 @@ if x==rev:
 else:
     print('not palin')
 
-
+n= int(input('enter a number:-'))
+count=0
+i=1
+while(i<=n):
+    if n%i==0:
+        count+=1
+    i+=1
+if count==2:
+    print('prime')
+else:
+    print('not prime')
 
 
 
