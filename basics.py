dde320af409150bce8abca97dc749cbb757fda86
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-09 17:47:44 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -271,6 +271,17 @@ while i <= n:
 print(sum)
 
 
+
+n = int(input("entera number:-"))
+sum = 0
+i = 1
+while i <= n:
+    if i % 2 == 0:
+        sum += i
+    i += 1
+print(sum)
+
+
 n = int(input("enter a number"))
 sum = 0
 i = 1
