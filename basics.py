585f0980969581003d63012d76007a63d46d8930
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-03-23 13:58:11 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1457,3 +1457,13 @@ nums = [1, 2, 2, 3, 4, 4, 5]
 
 unique = list(set(nums))
 print("Without duplicates:", unique)
+
+
+s = "hello world"
+
+count = 0
+for ch in s.lower():
+    if ch in "aeiou":
+        count += 1
+
+print("Vowels:", count)
