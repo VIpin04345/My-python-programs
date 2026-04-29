67747c01114bc23080bd9a44af0486052c93f30e
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-29 18:54:08 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -69,6 +69,11 @@ height=float(input('enter the hight of trangle'))
 width=float(input('enter the width of the trangle'))
 area=(1/2)*height*width
 print('the area of ractangle is ',area)
+
+height=float(input('enter the hight of trangle'))
+width=float(input('enter the width of the trangle'))
+area=(1/2)*height*width
+print('the area of ractangle is ',area)
 # (solution using math module)
 
 import math
